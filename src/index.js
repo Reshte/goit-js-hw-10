94d2273914121625f7349b4a103a6104295a8df0
Reshte <107108359+Reshte@users.noreply.github.com>
2022-11-21 09:04:58 +0200

    edits

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputEl: document.querySelector('[search-box]'),
+  ulEl: document.querySelector('.country-list'),
+  countryEl: document.querySelector('.country-info'),
+};
